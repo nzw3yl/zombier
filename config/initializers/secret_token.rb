0b9e87ebf82e17c4b87a11957839b703c0eef5bc
nzw3yl <roger.smith@ceryx.com>
2013-05-18 21:11:31 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zombier::Application.config.secret_token = '3e517a5290c53f51437f0ac5d063dbe50920377fbede533d121ecd4d2ef161ed03e9120a42150f1e5957455855115adcdf4964e3d4d7c84a86432a1f73657e1e'
